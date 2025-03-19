9c0ca2fc3d09f88406a9ffa087680c5458b3e621
unknown <cadavida920@gmail.com>
2025-03-18 23:18:57 -0500

    first commit

---
+++ src/app/pages/bancolombia/bancolombia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bancolombia',
+  imports: [],
+  templateUrl: './bancolombia.component.html',
+  styleUrl: './bancolombia.component.css'
+})
+export class BancolombiaComponent {
+
+}
